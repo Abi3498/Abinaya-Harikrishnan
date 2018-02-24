5a95929a3d0cff3021f0649fc9be166ea726d5fe
Abi3498 <36623738+Abi3498@users.noreply.github.com>
2018-02-24 19:57:20 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class p
+{
+    public static void main(String args[])
+    {
+        int a,b,c;
+        Scanner sr=new Scanner(System.in);
+        a=10;
+        b=20;
+        c=a+b;
+        System.out.println("Addition is:"+c);
+    }
+}
